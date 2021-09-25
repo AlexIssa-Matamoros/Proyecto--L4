270c0f956ad6d3802217be880cb0c791b8f0d8e0
Issa Matt <Issa Matt@DESKTOP-0SA65VC>
2021-09-25 15:30:02 -0600

    CAMBIOS

--- Electronica/Electronica.BL/ProductosBL.cs
+++ Electronica/Electronica.BL/ProductosBL.cs
@@ -8,31 +8,18 @@ namespace Electronica.BL
 {
     public class ProductosBL
     {
+        Contexto _contexto;
+
+        public ProductosBL()
+        {
+            _contexto = new BL.Contexto();
+        }
+
+
         public List<Producto> ObtenerProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Computadora";
-            producto1.Precio = 12000;
 
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Play Station 5";
-            producto2.Precio = 15000;
-
-            var producto3= new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Iphone X";
-            producto3.Precio = 10000;
-
-
-
-            var listaProductos = new List<Producto>();
-            listaProductos.Add(producto1);
-            listaProductos.Add(producto2);
-            listaProductos.Add(producto3);
-
-            return listaProductos;
+            return _contexto.Productos.ToList();
         }
     }
 }
